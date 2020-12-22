3dd75e3924f2646fedc547560487ea7b761a39b9
MtheEPIC <mtheepic1423@protonmail.com>
2020-12-22 16:23:32 +0200

    main func decleration

---
+++ myisort.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+// main funcs
+void insertion_sort(int* arr, int len); //sort the given array
+void shift_element(int* arr, int i); //shift all array pointer from a given point
+
+// helper funcs
+
