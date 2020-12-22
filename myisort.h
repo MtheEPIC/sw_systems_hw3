46045e56ad54b6684345976e44d14eaaad792101
MtheEPIC <mtheepic1423@protonmail.com>
2020-12-22 18:18:29 +0200

    more helper funcs

--- myisort.h
+++ myisort.h
@@ -5,4 +5,5 @@ void insertion_sort(int* arr, int len); //sort the given array
 void shift_element(int* arr, int i); //shift all array pointer from a given point
 
 // helper funcs
-
+void switch_elements(int* arr, int amount); //switch the arr pointer with arr+amount
+void print_array(int* arr, int len); //print array
