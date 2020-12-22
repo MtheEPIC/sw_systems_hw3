5bebd83fa52c4c4dcc59a0405392df3bf77e2e52
MtheEPIC <mtheepic1423@protonmail.com>
2020-12-22 20:07:47 +0200

    main func declaration

---
+++ mytxtfind.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// main funcs
+int getLine(char s[]); // get chars to create a string, return length, #define LINE 256
+int getword(char w[]); // get chars to create a string, return length, #define WORD 30
+int substring(char* str1, char* str2); // checks if str1 contains str2
+int similar(char* s, char* t, int n); // checks if by removing n chars from s you get t, retrun 1/0 (t,f)
+void print_lines(char* str); // gets keyword and prints line with the keyword
+void print_similar_words(char* str); // gets keyword and prints all similar words
+
+// helper funcs
